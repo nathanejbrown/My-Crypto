a00c02da2b1371c74d93ecde0f7e7665f4a5b5ef
Heathcliff <dennis.nathan@gmail.com>
2018-08-17 12:37:51 -0600

    Started tracking participants.

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Nathan'
+participants = {'Nathan'}
 
 def hash_block(block):
     return '-'.join([str(block[key]) for key in block])
@@ -25,6 +26,8 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         'recipient': recipient,
         'amount': amount}
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
 
 
 def mine_block():
@@ -71,6 +74,7 @@ while True:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output participants')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -83,6 +87,8 @@ while True:
         mine_block()
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
