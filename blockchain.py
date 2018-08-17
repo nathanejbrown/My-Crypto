650cbe3bca7a9e00eff9715db82ab1ff22d694f7
Heathcliff <dennis.nathan@gmail.com>
2018-08-17 16:18:38 -0600

    Added function to check validity of all open transactions

--- blockchain.py
+++ blockchain.py
@@ -100,6 +100,9 @@ def verify_chain():
             return False
     return True
 
+def verify_transactions():
+    return all([verify_transaction(tx) for tx in open_transactions])
+
 
 while True:
     print('Please choose')
@@ -107,6 +110,7 @@ while True:
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
     print('4: Output participants')
+    print('5: Check transaction validity')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -125,6 +129,11 @@ while True:
         print_blockchain_elements()
     elif user_choice == '4':
         print(participants)
+    elif user_choice == '5':
+        if verify_transactions():
+            print('All transactions are valid')
+        else:
+            print('There are invalid transactions')
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
