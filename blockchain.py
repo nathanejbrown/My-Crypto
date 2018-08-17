ccd7a01612d00d9f327c33229175fee6fce0b7cf
Heathcliff <dennis.nathan@gmail.com>
2018-08-17 10:45:25 -0600

    Added logic to prevent user from modifying the blockchain

--- blockchain.py
+++ blockchain.py
@@ -24,29 +24,53 @@ def add_transaction(transaction_amount, last_transaction):
 def get_transaction_value():
     return float(input('Your transaction amount please: '))
 
+
 def get_user_choice():
     user_input = input('Your choice: ')
     return user_input
+
 
 def print_blockchain_elements():
     for block in blockchain:
         print('Outputting block')
         print(block)
 
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else: 
+            is_valid = False
+            break
+    return is_valid
+
 while True:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_transaction(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+        add_transaction(last_transaction=get_last_blockchain_value(),
+                        transaction_amount=tx_amount)
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
         break
-    else: 
+    else:
         print('Invalid choice')
+    if not verify_chain():
+        print('Invalid blockchain')
+        break
 
 print('Done')
