482f079d609304b2d5b5a9054afae18c0c01f410
Heathcliff <dennis.nathan@gmail.com>
2018-08-21 11:23:39 -0600

    Created practice folder to store all files unrelated to the blockchain.

--- blockchain.py
+++ blockchain.py
@@ -17,6 +17,10 @@ open_transactions = []
 owner = 'Nathan'
 participants = {'Nathan'}
 
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        
+
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
     guess_hash = hash_string_256(guess)
