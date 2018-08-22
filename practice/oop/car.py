502c4ff5a846edf20e8fccaca1617993e02665a5
Heathcliff <dennis.nathan@gmail.com>
2018-08-22 12:04:33 -0600

    Started working with private attributes in Car class.

--- practice/oop/car.py
+++ practice/oop/car.py
@@ -3,11 +3,19 @@ class Car:
     # warnings = []
     def __init__(self, starting_top_speed=100):
         self.top_speed = starting_top_speed
-        self.warnings = []
+        # The double underscore here tells python that the variable should be private. 
+        self.__warnings = []
 
     def __repr__(self):
         print('Printing...')
-        return 'Top speed: {}, warnings: {}'.format(self.top_speed, self.warnings)
+        return 'Top speed: {}, __warnings: {}'.format(self.top_speed, self.__warnings)
+
+    def add_warning(self, warning_text):
+        if len(warning_text) > 0:
+            self.__warnings.append(warning_text)
+
+    def get_warnings(self):
+        return self.__warnings
 
     def drive(self):
         print('I am driving, but not faster than {}'.format(self.top_speed))
@@ -15,13 +23,14 @@ class Car:
 car1 = Car()
 car1.drive()
 
-car1.warnings.append('new warning')
+car1.add_warning('new warning')
+# car1.__warnings.append('blah')
 print(car1)
 
 car2 = Car(200)
 car2.drive()
 
-print(car2.warnings)
+print(car2.get_warnings())
 
 car3 = Car(300)
 car3.drive()
