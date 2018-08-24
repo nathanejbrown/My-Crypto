37fd1f70d76ca1f47b55fab5d83479f12641873d
Heathcliff <dennis.nathan@gmail.com>
2018-08-24 11:09:29 -0600

    Added printable.py file and had block and transaction inherit from it so that blocks get outputted in a readable way.

--- blockchain.py
+++ blockchain.py
@@ -12,7 +12,6 @@ MINING_REWARD = 10
 blockchain = []
 open_transactions = []
 owner = 'Nathan'
-participants = {'Nathan'}
 
 def load_data():
     global blockchain
@@ -159,8 +158,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
-    print('4: Output participants')
-    print('5: Check transaction validity')
+    print('4: Check transaction validity')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
@@ -178,8 +176,6 @@ while waiting_for_input:
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
-        print(participants)
-    elif user_choice == '5':
         if verify_transactions():
             print('All transactions are valid')
         else:
