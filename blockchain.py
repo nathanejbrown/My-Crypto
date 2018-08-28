2b8914b52efede4a4430c90aea59f42d5a6e1d41
Heathcliff <dennis.nathan@gmail.com>
2018-08-28 11:57:09 -0600

    Created logic in add_transaction in blockchain.py to broadcast new transaction information to all other running nodes. Still need to set up a route for that in node.py

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,7 @@ import functools
 import hashlib as hl
 import json
 import pickle
+import requests
 
 from utility.hash_util import hash_block
 from utility.verification import Verification
@@ -116,6 +117,15 @@ class Blockchain:
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
             self.save_data()
+            for node in self.__peer_nodes:
+                url = 'http://{}/broadcast-transaction'.format(node)
+                try:
+                    response = requests.post(url, json={'sender': sender, 'recipient': recipient, 'amount': amount, 'signature': signature})
+                    if response.status_code == 400 or response.status_code == 500:
+                        print('Transaction declined, needs resolving.')
+                        return False
+                except requests.exceptions.ConnectionError:
+                    continue
             return True
         return False
 
